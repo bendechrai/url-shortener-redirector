f27ddfd1a45e2152d5298312183fa683c922a4a3
Ben Dechrai <ben@dechrai.com>
2019-11-13 16:42:08 +1100

    Phase 1: redirect if the destination can be looked up

---
+++ api/redirect.js
@@ -0,0 +1,35 @@
+var request = require('request')
+
+module.exports = (req, res) => {
+
+  // Get the URL and strip any slashes
+  let url = req.url
+  url = url.replace('/', '')
+
+  // If URL is blank, we want the default redirect
+  if(url=='') url='__default__'
+
+  // Load data from JSON Box
+  url = 'https://jsonbox.io/' + process.env.urlshortener_jsonbox + '/' + url
+  request(url, { json: true }, (jb_err, jb_res, jb_body) => {
+
+    // If JSON Box has a response
+    if(jb_body.length==1) {
+
+      // And the response has a destination
+      if('dest' in jb_body[0]) {
+        res.writeHead(302, {"Location": jb_body[0].dest})
+        res.end()
+
+      // No destination? Nothing to do
+      } else {
+        res.send('Not found')
+      }
+
+    // No response? Nothing to do
+    } else {
+      res.send('Not found')
+    }
+  });
+
+}
